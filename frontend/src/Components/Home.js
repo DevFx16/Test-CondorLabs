1acc852c8ebb27382e74ed2783a873b9a33a6b8a
Fernando Araujo <developgadget@gmail.com>
2019-08-29 20:55:28 -0500

    Frontend

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import ListMessges from './ListMessges';
+import ListConversations from './ListConversations';
+import Message from './Message';
 
 function Home() {
 
     return (
         <div className="container-fluid h-100 animated fadeIn h-100">
             <div className="row h-100">
-                <nav className="col-md-3 col-lg-2 d-none d-md-block bg-light sidebar gradient">
+                <nav className="col-md-3 col-lg-2 d-none d-md-block bg-light sidebar gradient ">
                     <div className="row pt-3 justify-content-center">
                         <div className="col-5">
                             <img src="https://image.flaticon.com/icons/svg/660/660611.svg" className="rounded-circle float-right" alt="Cinque Terre" />
@@ -25,6 +26,29 @@ function Home() {
                             </div>
                         </div>
                     </div>
+                    <div className="row w-100 justify-content-between mx-0 pt-3">
+                        <div className="col">
+                            <button type="button" className="btn btn-link text-white" onClick={() => console.log('Hol')}>
+                                <i className="fas fa-users fa-lg"></i>
+                            </button>
+                        </div>
+                        <div className="col"></div>
+                        <div className="col">
+                            <button type="button" className="btn btn-link text-white">
+                                <i className="fas fa-user-cog fa-lg"></i>
+                            </button>
+                        </div>
+                    </div>
+                    <div className="row pt-3 justify-content-center">
+                        <div className="input-group w-75">
+                            <input className="form-control py-2 border-right-0 border bg-transparent text-white" type="search" placeholder="Seek conversation" />
+                            <span className="input-group-append">
+                                <div className="btn btn-link border-left-0 border text-white">
+                                    <i className="fa fa-search"></i>
+                                </div>
+                            </span>
+                        </div>
+                    </div>
                     <div className="row pt-3 justify-content-center">
                         <div className="accordion w-100" id="accordionExample">
                             <div className="card bg-transparent w-100">
@@ -36,10 +60,9 @@ function Home() {
                                         </button>
                                     </h2>
                                 </div>
-
                                 <div id="collapseOne" className="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
-                                    <div className="card-body px-0">
-                                        <ListMessges></ListMessges>
+                                    <div className="card-body px-0 pt-0 pb-0">
+                                        <ListConversations></ListConversations>
                                     </div>
                                 </div>
                             </div>
@@ -53,23 +76,16 @@ function Home() {
                                     </h2>
                                 </div>
                                 <div id="collapseTwo" className="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
-                                    <div className="card-body px-0">
-                                        <ListMessges></ListMessges>
+                                    <div className="card-body px-0 pt-0 pb-0">
+                                        <ListConversations></ListConversations>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div className="row align-bottom">
-                        <div className="card bg-transparent px-0">
-                            <div className="card-body">
-
-                            </div>
-                        </div>
-                    </div>
                 </nav>
                 <main role="main" className="col-md-9 ml-sm-auto col-lg-10 pt-3 px-4">
-                    <div className="card bg-transparent">
+                    <div className="card bg-transparent chat">
                         <div className="card-header border-shadow">
                             <div className="row">
                                 <div className="col">
@@ -78,8 +94,18 @@ function Home() {
                                         <p className="font-weight-bold text-white mb-0 ml-2">KillGamer16</p>
                                     </div>
                                 </div>
-                                <div className="col">
-
+                            </div>
+                        </div>
+                        <div className="card-body border-shadow overflow-auto">
+                            {
+                                [1,2,2,3,4,5,6,6,7,1,3,5,2].map((item,index) => <Message></Message>)
+                            }
+                        </div>
+                        <div className="card-footer">
+                            <div className="input-group">
+                                <textarea name="" className="form-control type_msg" placeholder="Type your message..."></textarea>
+                                <div className="input-group-append">
+                                    <span className="input-group-text send_btn"><i className="fas fa-location-arrow"></i></span>
                                 </div>
                             </div>
                         </div>
---
+++ frontend/src/Components/Message.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Message() {
+    return (
+        <div className="row">
+            <div className="col-1 px-0 text-center">
+                <img src="https://image.flaticon.com/icons/svg/660/660611.svg" className="rounded-circle ml-2" alt="Cinque Terre" height={40} width={40} />
+            </div>
+            <div className="col">
+                <div className="row">
+                    <p className="font-weight-bold text-white mb-0 ml-2">KillGamer16</p>
+                </div>
+                <div className="row">
+                    <p className="text-break text-white mb-0 ml-2">HolaMundoHolaMundoHolaMundoHolaMundoHolaMundoHolaMundoHolaMundoHolaMundoHolaMundoHolaMundoHolaMundo</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Message
